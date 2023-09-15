b7dd7b6b89f3a31553f41d094e8d72ebe5d1f1d2
Rhys Pillay <Rhys.pillay@gmail.com>
2023-09-15 14:36:29 +0200

    IWA2 Challenge Solutions

---
+++ IWA2 Challenge Solutions/script.js
@@ -0,0 +1,36 @@
+// Challenge 1 Solution
+
+// This is the firstname of the user
+
+const user = "John";
+
+// /* This is the lastname of the user /*
+
+const surname = "Smith";
+
+console.log(user, surname);
+
+// Should use Commas instead of semi colons
+
+// Challenge 2 Solution
+
+const date = "10/07/2014";
+
+console.log(date);
+
+// Was missing the "log"
+
+// Challenge 3 Solution
+
+// It is important to show the following message in order to:
+// - to assure users
+// - to scare hackers
+// - to impress investors
+
+console.warn("Security scan starting");
+
+// It is important to let user know when they can close the page
+
+console.info("Please wait for scan to complete before closing the browser.");
+
+// The opening and closing tags were missing and the spelling of the word console was incorrect
